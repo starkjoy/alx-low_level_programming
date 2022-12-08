67d6db34f1c8d6a7298e15f2e936be7aa6017804
stark stein <residualcode6@gmail.com>
2022-12-08 11:00:08 -0800

    Saved executable to file

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,10 +2,11 @@
 
 /**
  *  main - Prints "Programming is like building a multilingual puzzle"
+ *  Return - returns 0
  */
 
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzl\n");
+	puts("\"Programming is like building a multilingual puzzle\n");
 	return (0);
 }
