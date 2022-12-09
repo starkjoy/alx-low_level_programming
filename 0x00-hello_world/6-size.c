f1f5f60f544a87474aa37690b279848861efa881
stark stein <residualcode6@gmail.com>
2022-12-09 02:55:47 -0800

    First C filec

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - main prints sizes of data types
+ *
+ * Return: returns 0
+ */
+
+int main(void)
+{
+	int a;
+	long int b;
+	long long int c;
+	char d;
+	float f;
+
+	printf("Size of int: %lu bytes(s)\n", (unsigned long)sizeof(a));
+	printf("Size of long int: %lu bytes(s)\n", (unsigned long)sizeof(b));
+	printf("Size of long long int: %lu bytes(s)\n", (unsigned long)sizeof(c));
+	printf("Size of char: %lu bytes(s)\n", (unsigned long)sizeof(d));
+	printf("Size of float: %lu bytes(s)\n", (unsigned long)sizeof(f));
+}
