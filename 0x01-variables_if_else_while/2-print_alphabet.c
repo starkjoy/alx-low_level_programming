81b216b0272cc502b159cfd07da642e9d741aa74
stark stein <residualcode6@gmail.com>
2022-12-09 11:47:55 -0800

    C conditional Statements

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - main prints in lower and upper case
+ *
+ * Return: return 0
+ */
+
+int main(void)
+{
+	char alpha;
+
+	for (alpha = 'a'; alpha <= 'z'; alpha++)
+	{
+		putchar(alpha)
+		if (alpha == 'z')
+		{
+			putchar('\n');
+		}
+	}
+	return (0);
+}
---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - main prints in lower and upper case
+ *
+ * Return: return 0
+ */
+
+int main(void)
+{
+	int base;
+
+	for (base = 0;  base <= 10 ; base++)
+	{
+		putchar(alpha)
+		if (base == 10)
+		{
+			putchar('\n');
+		}
+	}
+	return (0);
+}
