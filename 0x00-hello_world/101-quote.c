6e83ccef9251e39c6d14601c2f897b34588c3e1b
stark stein <residualcode6@gmail.com>
2022-12-09 03:44:09 -0800

    First C filec

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 #include <unistd.h>
 /**
- * main - main prints an error to STDOUT
+ * main - main prints with write()
  *
  * Return: returns 1
  */
 
 int main(void)
 {
-	write(1, "and that piece of art is useful \" - Dora Korpar, 2015-10-19\n", 59);
+	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
