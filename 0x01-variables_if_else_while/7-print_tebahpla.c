56533ab885bfa95570cb5be928a23d7b01f4fd18
stark stein <residualcode6@gmail.com>
2022-12-09 12:50:10 -0800

    C conditional Statements

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - main prints in lower case in reverse
+ *
+ * Return: return 0
+ */
+
+int main(void)
+{
+	int base;
+
+	for (base = 'z';  base <= 'a'; base--)
+	{
+		putchar(base);
+		if (base == 'z')
+		{
+			putchar('\n');
+		}
+	}
+
+	return (0);
+}
