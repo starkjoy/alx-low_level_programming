e278a2e12e907dff65a4fcfbc191eeca8f1b7bd2
stark stein <residualcode6@gmail.com>
2022-12-09 08:42:25 -0800

    C conditional Statements

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -16,7 +16,7 @@ int main(void)
 	n = rand() - RAND_MAX / 2;
 	if (n > 5)
 	{
-		printf("Last digit of %d is %i is greater than 5", n);
+		printf("Last digit of %d is %i is greater than 5", n,n);
 	}
 	if (n == 0)
 	{
