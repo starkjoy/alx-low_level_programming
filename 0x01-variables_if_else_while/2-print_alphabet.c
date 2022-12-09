fd14395e3ea433b23904d827588b01dd881c244c
stark stein <residualcode6@gmail.com>
2022-12-09 10:31:26 -0800

    C conditional Statements

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - main prints the alphabet in lowercase
+ *
+ * Return: returns 0
+ */
+
+int main (void)
+{
+	char alpha;
+
+	for (alpha = 'a'; alpha <= 'z'; alpha ++)
+	{
+		putchar(alpha);
+		putchar('\n');
+	}
+
+	return (0);
+}
