eb44186d08f7c1122b45e09e723cfb2eba4ad8a9
stark stein <residualcode6@gmail.com>
2022-12-09 13:12:06 -0800

    C conditional Statements

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - main prints possible combinations of single-digits
+ *
+ * Return: return 0
+ */
+
+int main(void)
+{
+	int base;
+
+	for (base = '0';  base <= '9'; base++)
+	{
+		putchar(base);
+		if (base == '9')
+		{
+			putchar('\n');
+		}
+		else
+		{
+			base = base + ", ";
+		}
+	}
+
+	return (0);
+}
