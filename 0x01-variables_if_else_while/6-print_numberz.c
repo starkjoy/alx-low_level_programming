288a95186e422cebd533e58526827247d5c097ee
stark stein <residualcode6@gmail.com>
2022-12-09 12:02:02 -0800

    C conditional Statements

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - main prints in lower and upper case
+ *
+ * Return: return 0
+ */
+
+int main(void)
+{
+	int base;
+
+	for (base = 0;  base < 10 ; base++)
+	{
+		putchar(base)
+		putchar('\n');
+	}
+	return (0);
+}
