7178abfae7b9720491f092c2bb4fff424d3fadaa
stark stein <residualcode6@gmail.com>
2022-12-09 08:45:12 -0800

    C conditional Statements

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -24,7 +24,7 @@ int main(void)
 	}
 	if (n < 6)
 	{
-		printf("Last digit of %d is %1 is less than 6 and not 0", n,n);
+		printf("Last digit of %d is %i is less than 6 and not 0", n,n);
 	}
 	return (0);
 }
