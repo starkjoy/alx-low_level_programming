9bfa45357627e39e605669424486376bbea3f424
stark stein <residualcode6@gmail.com>
2022-12-09 02:57:23 -0800

    First C filec

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -19,4 +19,5 @@ int main(void)
 	printf("Size of long long int: %lu bytes(s)\n", (unsigned long)sizeof(c));
 	printf("Size of char: %lu bytes(s)\n", (unsigned long)sizeof(d));
 	printf("Size of float: %lu bytes(s)\n", (unsigned long)sizeof(f));
+	return (0);
 }
