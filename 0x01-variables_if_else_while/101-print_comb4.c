884c2398c6d7fca633eb244945820b4b10fe575e
stark stein <residualcode6@gmail.com>
2022-12-12 01:23:52 -0800

    Conditional Statements

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+/**
+ * main - main printsn combinations of two numbers
+ *
+ * Return: return 0
+ */
+
+int main(void)
+{
+	int b;
+	int c;
+	int d;
+
+	for (b = '0';  b < '9'; b++)
+	{
+		for (c = 1; c <= '9'; c++)
+		{
+			for (d = '1'; d <= '9'; d++)
+			{
+				if ((d > c) && c < b)
+				{
+					putchar(b);
+					putchar(c);
+
+					if (b == '7' && c == '8')
+					continue;
+
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+
+
+	return (0);
+}
