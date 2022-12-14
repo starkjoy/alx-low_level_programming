2eeef86c19d3c91693112b52b6716ed227374781
stark stein <residualcode6@gmail.com>
2022-12-14 08:28:00 -0800

    Added C-fies on functions, nested loops

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <ctype.h>
 #include "main.h"
 
 /**
@@ -12,16 +11,9 @@
  */
 
 int print_last_digit(int i)
-{
-	if (isfloat(i))
-	{
-		i = abs(i) % 10;
-		_putchar(i + '0');
-	}
-	else
-	{
-		_putchar((i / 10) % 10 + '0');
-	}
+{	
+	i = abs(i) % 10;
+	_putchar(i + '0');
 
 	return (i);
 }
