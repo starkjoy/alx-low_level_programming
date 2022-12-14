2f062b5d3f343e0c2c7053c70a8485c9d805ab1f
stark stein <residualcode6@gmail.com>
2022-12-14 08:50:02 -0800

    Added C-fies on functions, nested loops

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -10,13 +10,13 @@ void times_table(void)
 {
 	int a;
 	int b;
-	int z;
 
 	for (a = 0; a <= 9; a++)
 	{
 		for (b = 0; b <= 9; b++)
 		{
-			zero = a * b;
+			int zero = a * b;
+
 			if (zero < 10)
 			{
 				putchar(zero + '0');
