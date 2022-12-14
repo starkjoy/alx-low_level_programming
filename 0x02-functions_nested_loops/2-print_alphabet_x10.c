f97872553d5161c0ea3bc015c00342f34db2c756
stark stein <residualcode6@gmail.com>
2022-12-14 02:06:21 -0800

    Added C-files on functions,nested loops

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,14 +1,7 @@
-#include "main.h"
 #include <stdio.h>
 
 /**
- * main - main prints a list of alphabets
- *
- * Return: returns 0
- *
  * print_alphabet_x10 - generates a 10x lowercase letters
- *
- * Return: returns 0 
  *
  */
 
@@ -30,10 +23,4 @@ void print_alphabet_x10(void)
 
 		b++;
 	}
-}
-
-int main(void)
-{
-	print_alphabet();
-	return (0);
 }
