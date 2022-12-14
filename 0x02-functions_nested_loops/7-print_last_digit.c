0ef937ce03c427cdc95e298f8b0b2d7325ee0b4f
stark stein <residualcode6@gmail.com>
2022-12-14 07:32:55 -0800

    Added C-fies on functions, nested loops

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-#include <stdlib.h>
+#include "main"
 
 /**
  * print_last_digit - prints exactly that
@@ -12,6 +12,8 @@
 int print_last_digit(int i)
 {
 
-	i = i % 10 * 11;
-	return(i);
+	i = i % 10;
+	_putchar(i + '0');
+
+	return(0);
 }
