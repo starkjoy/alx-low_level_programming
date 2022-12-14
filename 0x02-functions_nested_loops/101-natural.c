96e87a6ee48d60d2bfd86ccad3e8250c59df5438
stark stein <residualcode6@gmail.com>
2022-12-14 14:39:27 -0800

    Added C-files on functions, nested loops

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: returns 0
+ */
+
+int main(void)
+{
+	int i, sum = 0;
+
+	for (i = 0; i < 1024; i++)
+	{
+		if ((i % 5) == 0 || (i % 3) == 0)
+			sum += i;
+	}
+	printf("%d\n", sum);
+
+	return(0);
+}
