d8d6dd0db9eec7769f5aa034cafddb2168404346
starkjoy <residualcode6@gmail.com>
2022-12-15 09:20:43 +0000

    Added C-scripts for debugging

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - tests if function works right
+ * Return: returns 0
+ *
+ */
+
+int main(void)
+{
+	int i;
+	
+	i = 0;
+	positive_or_negative(i);
+	
+	return (0);
+}
