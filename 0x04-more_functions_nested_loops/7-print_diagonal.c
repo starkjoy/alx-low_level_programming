c04b09ba86c5f3bee76fde6a87cc95967f397b08
stark stein <residualcode6@gmail.com>
2022-12-16 05:54:37 -0800

    Added more functions and loops in C

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -13,13 +13,20 @@ void print_diagonal(int n)
 	int i;
 	int j;
 
-	for (i = 0; i < n; i++)
+	if (n > 0)
 	{
-		for (j = 0; j < i; j++)
+		for (i = 0; i < n; i++)
 		{
-			_putchar(' ');
+			for (j = 0; j < i; j++)
+			{
+				_putchar(' ');
+			}
+			_putchar('\\');
+			_putchar('\n');
 		}
-		_putchar('\\');
+	}
+	else if (n <= 0)
+	{
 		_putchar('\n');
 	}
 }
