9c96a82fbf27c2ae762af427498b89378476c577
stark stein <residualcode6@gmail.com>
2022-12-16 11:59:24 -0800

    Added more functions and loops in C

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -16,7 +16,7 @@ void print_triangle(int size)
 
 	if (size > 0)
 	{
-		for (i = 0; i < size; i++)
+		for (i = 0; i <= size; i++)
 		{
 			for (j = 0; j < size - i - 1; j++)
 			{
---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * Description: It computes a factor
+ * Return: 0
+ */
+
+int main(void)
+{
+
+	return (0);
+}
