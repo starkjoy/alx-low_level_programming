e8eba11913023d5f59118d4805be298c32ae26df
stark stein <residualcode6@gmail.com>
2022-12-16 08:23:01 -0800

    Added more functions and loops in C

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_triangle - draws a triangle with #
+ * Description: draws a triangle
+ * @size: accepts integer numbers
+ * Return: void
+ */
+
+void print_triangle(int size)
+{
+	int i;
+	int j;
+	int k;
+
+	if (size > 0)
+	{
+		for (i = 0; i < size; i++)
+		{
+			for (j = 0; j < h - i - 1; j++)
+			{
+				_putchar(' ');
+			}
+			for (k = 0; k < 1 + 1; k++)
+			{
+				_putchar('#');
+			}
+		}
+	}
+	else if (size <= 0)
+	{
+		_putchar('\n');
+	}
+}
