ee0a2a2693c178c3935807da5cf56adc18a3ef67
stark stein <residualcode6@gmail.com>
2022-12-16 04:20:49 -0800

    Added more functions and loops in C

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_line - draws a straight line
+ * Description: draws a line
+ * @n: accepts integer Characters
+ * Return: void
+ */
+
+void print_line(int n)
+{
+	if (n > 0)
+	{
+		int j;
+
+		for (j = 0; j < n; j++)
+		{
+			_putchar ('_');
+		}
+	}
+	else
+	{
+		_putchar('\n');
+	}
+	putchar('\n');
+}
