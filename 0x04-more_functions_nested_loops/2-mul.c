35ef9ce86fe358e085d6b68689db056a24099509
stark stein <residualcode6@gmail.com>
2022-12-16 02:32:12 -0800

    Added more functions and loops in C

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * mul- multiplies two numbers
+ * Description: Multiplies numbers
+ * @a: First number
+ * @b: Second number
+ * Return: 0
+ */
+
+int mul(int a, int b)
+{
+	int c;
+
+	c = a * b;
+
+	return (c);
+}
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,4 +1,5 @@
 int _isupper(int c);
 void _putchar(void);
 int _isdigit(int c);
+int mul(int a, int b);
 
