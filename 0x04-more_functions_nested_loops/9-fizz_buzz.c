aa6e33d0bf44cea53ee744c96e9b839823fcc8a8
stark stein <residualcode6@gmail.com>
2022-12-16 07:03:33 -0800

    Added more functions and loops in C

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -12,15 +12,20 @@ int main(void)
 
 	for (i = 1; i <= 100; i++)
 	{
-		if (i % 3 == 0)
+		if (i % 3 == 0 && i % 5 != 0)
 		{
 			printf("Fizz");
 			printf(" ");
 		}
-		else if (i != 100 && i % 5 == 0)
+		else if (i != 100 && i % 5 == 0 && i % 3 != 0)
 		{
 			printf("Buzz");
 			printf(" ");
+		}
+		else if (i % 3 == 0 && i % 5 == 0)
+		{
+			printf("FizzBuzz");
+			putchar(' ');
 		}
 		else
 		{
