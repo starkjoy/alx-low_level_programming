fb57f4d72fbdeda79bd7c1591aec0fda735bb10c
stark stein <residualcode6@gmail.com>
2022-12-16 06:08:18 -0800

    Added more functions and loops in C

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_square - draws a square
+ * Description: draws a square
+ * @size: accepts integer
+ * Return: void
+ */
+
+void print_square(int size)
+{
+	int i;
+	int j;
+
+	if (n > 0)
+	{
+		for (i = 0; i < size; i++)
+		{
+			for (j = 0; j < (size - 1); j++)
+			{
+				_putchar('#');
+			}
+			_putchar('#');
+			_putchar('\n');
+		}
+	}
+	else if (n <= 0)
+	{
+		_putchar('\n');
+	}
+}
