95cf344bed2275933b68a6ed6e5954529b9b2866
stark stein <residualcode6@gmail.com>
2022-12-16 15:38:27 -0800

    Added more C files on functions and loops

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_number - prints an integer
+ * Description: It returns 1 for uppercase or 0 for non uppercase
+ * @n: accepts integer
+ * Return: 0
+ */
+
+void print_number(int n)
+{
+	if (n < 0)
+	{
+		_putchar('-');
+		n = -n;
+		_putchar('\n');
+	}
+	if (n / 10)
+	{
+		print_number(n / 10);
+	}
+	_putchar(n % 10 + '0');
+}
