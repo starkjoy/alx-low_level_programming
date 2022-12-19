dffc33e180fda0202cac6e0163a3801f8f3155cc
stark stein <residualcode6@gmail.com>
2022-12-19 09:11:35 -0800

    Pointers, Arrays, Strings

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -1,46 +1,49 @@
 #include <stdio.h>
-#include <math.h>
 #include <stdlib.h>
 #include <time.h>
 
 /**
  * main - Program Entry
+ * generate_random_char - generates random number
  * Description: Generates random valid passwords for program 101-crackme.
  * Return: 0
 */
 
+char generate_random_char(void);
+
 int main(void)
 {
-	int i = 0;
-	int randomizer = 0;
-	unsigned int seed;
+	char password[100];
 
-	char numbers[] = "0123456789";
-	char letter[] = "abcdefghijklmnopqrstuvwxyz";
-	char LETTER[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-	char symbols[] = "!@#$^&*?";
-	char password[];
+	srand(time(NULL));
 
-	randomizer = rand() % 4;
-
-	if (randomizer == 1)
+	for (int i = 0; i < 100; i++)
 	{
-		password[i] = numbers[rand() % 10];
-		randomizer = rand() % 4;
-		printf("%c", password[i]);
+		password[i] = generate_random_char();
 	}
-	else if (randomizer == 2)
+
+	password[99] = '\0';
+
+	printf("%s\n", password);
+
+	return (0);
+
+}
+
+char generate_random_char(void)
+{
+	int r = rand() % 62;
+
+	if (r < 26)
 	{
-		password[i] = symbols[rand() % 8];
-		randomizer = rand() % 4;
-		printf("%c", password[i]);
+		return ('a' + r);
+	}
+	else if (r < 52)
+	{
+		return ('A' + (r - 26));
 	}
 	else
 	{
-		password[i] = letter[rand() % 26];
-		randomizer = rand() % 4;
-		printf("%c", password[i]);
+		return ('0' + (r - 52));
 	}
-
-	return (0);
 }
