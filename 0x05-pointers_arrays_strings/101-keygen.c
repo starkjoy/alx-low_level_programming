73d18692250c4abaac58375f361fb1dea42fa389
stark stein <residualcode6@gmail.com>
2022-12-19 08:14:18 -0800

    Pointers, Arrays, Strings

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -15,8 +15,6 @@ int main(void)
 	int randomizer = 0;
 	unsigned int seed;
 
-	srand((seed)(time(NULL)));
-
 	char numbers[] = "0123456789";
 	char letter[] = "abcdefghijklmnopqrstuvwxyz";
 	char LETTER[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
@@ -25,26 +23,23 @@ int main(void)
 
 	randomizer = rand() % 4;
 
-	for (i = 0; i < N; i++)
+	if (randomizer == 1)
 	{
-		if (randomizer == 1)
-		{
-			password[i] = numbers[rand() % 10];
-			randomizer = rand() % 4;
-			printf("%c", password[i]);
-		}
-		else if (randomizer == 2)
-		{
-			password[i] = symbols[rand() % 8];
-			randomizer = rand() % 4;
-			printf("%c", password[i]);
-		}
-		else
-		{
-			password[i] = letter[rand() % 26];
-			randomizer = rand() % 4;
-			printf("%c", password[i]);
-		}
+		password[i] = numbers[rand() % 10];
+		randomizer = rand() % 4;
+		printf("%c", password[i]);
+	}
+	else if (randomizer == 2)
+	{
+		password[i] = symbols[rand() % 8];
+		randomizer = rand() % 4;
+		printf("%c", password[i]);
+	}
+	else
+	{
+		password[i] = letter[rand() % 26];
+		randomizer = rand() % 4;
+		printf("%c", password[i]);
 	}
 
 	return (0);
