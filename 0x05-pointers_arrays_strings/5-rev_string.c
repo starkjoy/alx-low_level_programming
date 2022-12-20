6897e61f76c2ccdc02b9d1431bceae98340ef72b
starkjoy <residualcode6@gmail.com>
2022-12-20 17:25:27 +0000

    Pointers, Arrays, Strings

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * rev_string - prints a reverse string
+ * Description: prints string
+ * @s: accepts a string
+ * Return: returns void
+ */
+
+void rev_string(char *s)
+{
+	int l = 0;
+
+	while (s[l] != '\0')
+	{
+		l++;
+	}
+	for (l -= 1; l >= 0; l--)
+	{
+		putchar(s[l]);
+	}
+	putchar('\n');
+}
