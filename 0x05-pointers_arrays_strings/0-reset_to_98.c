d3fc2d16384ff62f55bb575e18afc779f6aa3647
stark stein <residualcode6@gmail.com>
2022-12-20 00:29:27 -0800

    Pointers, Arrays and Strings

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * reset_to_98 - reset integer at pointer
+ * Description: Resets to 98
+ * @*n: accepts pointer to integer
+ * Return: void
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
