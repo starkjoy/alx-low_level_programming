d98e096fd0d6a07acac30bc59d8bb67bcec010f4
stark stein <residualcode6@gmail.com>
2022-12-20 01:30:48 -0800

    Pointers, Arrays and Strings

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * _strlen - display length of string
+ * Description: lenght of string
+ * @s: accepts a character
+ * Return: returns an integer
+ */
+
+int _strlen(char *s)
+{
+	int char_length = strlen(*s);
+
+	return char_lenght;
+}
