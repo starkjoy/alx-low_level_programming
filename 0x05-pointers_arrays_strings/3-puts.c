db183f737a94e4e2a7910795942735a5adfe7e18
stark stein <residualcode6@gmail.com>
2022-12-20 02:03:46 -0800

    Pointers, Arrays and Strings

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * _puts - prints a string
+ * Description: prints string
+ * @str: accepts a string
+ * Return: returns void
+ */
+
+void _puts(char *str)
+{
+	int strings;
+
+	strings = str;
+	puts(strings);
+}
