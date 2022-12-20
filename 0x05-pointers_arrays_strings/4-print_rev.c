109cc9c9357ccbaa552c9f200306e2d861cb3223
stark stein <residualcode6@gmail.com>
2022-12-20 04:18:14 -0800

    Pointers, Arrays, Strings

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * print_rev - prints a reverse string
+ * Description: prints string
+ * @s: accepts a string
+ * Return: returns void
+ */
+
+void print_rev(char *s)
+{
+	int length;
+	int start;
+	int end;
+
+	length = strlen(s);
+	start = 0;
+	end = length - 1;
+
+	while (start < end)
+	{
+		char temp;
+
+		temp = s[start];
+		s[start] = s[end];
+		s[end] = temp;
+
+		start++;
+		end--;
+	}
+}
