10bb88168013a03931da50f573cb9f009f98d4a6
stark stein <residualcode6@gmail.com>
2022-12-20 00:38:29 -0800

    Pointers, Arrays and Strings

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * swap_int - swaps integer
+ * Description: swaps two integers
+ * @a: accepts pointer to first integer
+ * @b: accepts pointer to second integer
+ * Return: void
+ */
+
+void swap_int(int *a, int *b)
+{
+	int b;
+	int c;
+
+	*a = &b;
+	*b = &c;
+}
