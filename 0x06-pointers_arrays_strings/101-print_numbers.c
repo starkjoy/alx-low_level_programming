f38226978e9225ff912fc41c6868e5da09b8c371
starkjoy <117626913+starkjoy@users.noreply.github.com>
2022-12-22 19:51:34 +0000

    Create 101-print_numbers.c

---
+++ 0x06-pointers_arrays_strings/101-print_numbers.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_number - prints an integer
+ * Description: It prints an integer using putchar
+ * @n: accepts integer
+ * Return:void
+ */
+
+void print_number(int n)
+{
+	if (n < 0)
+	{
+		_putchar('-');
+		n = -n;
+	}
+
+	if (n / 10)
+	{
+		print_number(n / 10);
+	}
+	_putchar(n % 10 + '0');
