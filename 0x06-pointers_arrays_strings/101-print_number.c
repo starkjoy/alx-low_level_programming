f07c989bcfa5517581cdf0083eb5f6070b810a14
stark stein <residualcode6@gmail.com>
2022-12-23 01:26:32 -0800

    More Pointers, Arrays and Strings

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <ctype.h>
 #include "main.h"
 /**
  * print_number - prints an integer
@@ -9,15 +10,18 @@
 
 void print_number(int n)
 {
-	if (n < 0)
+	if (!(isalpha(n)))
 	{
-		_putchar('-');
-		n = -n;
-	}
+		if (n < 0)
+		{
+			_putchar('-');
+			n = -n;
+		}
 
-	if (n / 10)
-	{
-		print_number(n / 10);
+		if (n / 10)
+		{
+			print_number(n / 10);
+		}
+		_putchar(n % 10 + '0');
 	}
-	_putchar(n % 10 + '0');
 }
