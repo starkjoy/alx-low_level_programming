46bf218391ea8357b3f2898533d57c66202d825e
stark stein <residualcode6@gmail.com>
2023-01-03 09:07:00 -0800

    Even more pointers, arrays and strings

---
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+/**
+ * print_diagsums - prints the sum of two square matrice diagonals
+ * Description: prints the sum of two matrice diagonals
+ * @a: accepts an integer array
+ * @size: accepts integer
+ * Return: returns void
+ */
+
+void print_diagsums(int *a, int size)
+{
+	int i;
+	int ans = 0;
+	int ans1 = 0;
+
+	for (i = 0; i < size; i++)
+	{
+		int j;
+
+		for (j = 0; j < size; j++)
+		{
+			if (i == j)
+			{
+				ans = ans + *(a + i * size + j);
+			}
+			if (i + j == (size - 1))
+			{
+				ans1 = ans1 + *(a + i * size + j);
+			}
+		}
+	}
+	printf("%i ", ans);
+	printf("%i\n", ans1);
+}
