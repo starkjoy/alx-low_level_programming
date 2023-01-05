c41097ba80830d84328ab27e4dba624b5fa798e4
stark stein <residualcode6@gmail.com>
2023-01-05 04:59:50 -0800

    Recursion files

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * _strlen_recursion - returns the length of a string
+ * Description: returns a string length
+ * @s: accepts a char array
+ * Return: returns an integer
+ */
+
+int _strlen_recursion(char *s)
+{
+	int len = strlen(s);
+
+	return (len);
+}
