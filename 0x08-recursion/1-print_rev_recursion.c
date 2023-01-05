092ebeac25fd64db8c71089286e2dec316a1a524
stark stein <residualcode6@gmail.com>
2023-01-05 04:50:13 -0800

    Recursion files

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * _print_rev_recursion - prints string in reverse
+ * Description: prints string in reverse
+ * @s: accepts a char array
+ * Return: returns nothing
+ */
+
+void _print_rev_recursion(char *s)
+{
+	if (*s == '\0');
+	else
+	{
+		_print_rev_recursion(s+1);
+
+		_putchar(*s);
+	}
+}
