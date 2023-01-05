b4368188e0cf5693efe763868ceb9ce940b4776d
stark stein <residualcode6@gmail.com>
2023-01-05 10:07:44 -0800

    Recursion files

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * _is_prime_number - returns 1 if the integer is prime
+ * Description: returns 1 for a prime numer
+ * @n: accepts an int
+ * Return: returns an integer
+ */
+
+int _is_prime_number(int n)
+{
+	int num;
+
+	if (num == i)
+	{
+		return (0);
+	}
+	else if (num % i == 0)
+	{
+		return (1);
+	}
+	else
+	{
+		return _is_prime_number(i + 1);
+	}
+}
