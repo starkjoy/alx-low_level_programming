be7e1a48156002f41cc16c5e66454095132ea343
stark stein <residualcode6@gmail.com>
2023-01-05 05:25:05 -0800

    Recursion files

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * _pow_recursion - returns value of x raised to the power y
+ * Description: returns a power of a number
+ * @x: accepts an int
+ * @y: accepts an int
+ * Return: returns an integer
+ */
+
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+	{
+		return (-1);
+	}
+	else if (y == 1)
+	{
+		return (1);
+	}
+	else
+	{
+		return (pow(x, y));
+	}
+}
--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -3,4 +3,5 @@ void _print_rev_recursion(char *s);
 void _putchar(char);
 int _strlen_recursion(char *s);
 int factorial(int n);
+int _pow_recursion(int x, int y);
 
