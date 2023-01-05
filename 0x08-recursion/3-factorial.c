3774a0bfa94bed5071a8f020f0456a943ef6f73b
stark stein <residualcode6@gmail.com>
2023-01-05 05:05:32 -0800

    Recursion files

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * factorial - returns factorial of a number
+ * Description: returns a factorial
+ * @n: accepts a char array
+ * Return: returns an integer
+ */
+
+int factorial(int n)
+{
+	if (n < 0)
+	{
+		return (0);
+	}
+	else
+	{
+		return (factorial(n - 1));
+	}
+}
