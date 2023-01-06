14cddea9fd7818ac8514def167c5439c28942df3
stark stein <residualcode6@gmail.com>
2023-01-06 01:01:17 -0800

    Recursive files

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 
+int check_prime(int n, int i);
+
 /**
  * _is_prime_number - returns 1 if the integer is prime
  * Description: returns 1 for a prime numer
@@ -9,18 +11,30 @@
 
 int _is_prime_number(int n)
 {
-	int num;
+	return (check_prime(n, 1));
+}
 
-	if (num == i)
+/**
+ * check_prime -Check if number is prime
+ * @n: the prime to be checked
+ * @i: the iteration times
+ *
+ * Return: 1 for prime or 0 composite
+ */
+
+int check_prime(int n, int i)
+{
+	if (n <= 1)
 	{
 		return (0);
 	}
-	else if (num % i == 0)
+	if (n % i == 0 && i > 1)
 	{
 		return (1);
 	}
-	else
+	if ((n / i) < i)
 	{
-		return _is_prime_number(i + 1);
+		return (1);
 	}
+	return (check_prime(n, i + 1));
 }
