f48d4f1a8db41e7cbf15cdf97fd88d0d1a163be2
stark stein <residualcode6@gmail.com>
2023-01-06 02:13:18 -0800

    Recursive files

---
+++ 0x08-recursion/101-wildcmp.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+/**
+ * wildcmp - compares two strings
+ * @s1: accepts a string array
+ * @a2: accepts a string  array
+ * Return: returns an integer
+ */
+
+int wildcmp(char *s1, char *s2)
+{
+	if (*s2 == '*' && *(s2 + 1) != '\0' && *s1 == '\0')
+	{
+		return (0);
+	}
+
+	if (*s1 == '\0' && *s2 == '\0')
+	{
+		return (1);
+	}
+
+	if (*s1 == *s2)
+	{
+		return (wildcmp(s1 + 1, s2 + 1));
+	}
+
+	if (*s2 == '*')
+	{
+		return (wildcmp(s1, s2 + 1) || wildcmp(s1 + 1, s2));
+	}
+
+	return (0);
+}
