5bff3dd76b87d1c9b2e538816add96453a8bc78a
stark stein <residualcode6@gmail.com>
2023-01-06 01:38:46 -0800

    Recursive files

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * palrec - checks for palindrome
+ * Description: checks for palindrome
+ * @s: acceps array
+ * @l: left integer
+ * @r: right integer
+ * Return: integer
+ */
+
+int palrec(char *s, int l, int r)
+{
+	if (l >= r)
+	{
+		return (1);
+	}
+
+	if (s[l] != s[r])
+	{
+		return (0);
+	}
+
+	return palrec(s, l + 1, r - 1);
+}
+
+/** 
+ * is_palindrome - returns 1 if a string is palindrome and 0 for the inverse
+ * Description: returns 1 if string is palindrome and 0 for the opposite
+ * @s: accepts an array
+ * Return: returns an integer
+ */
+
+int is_palindrome(char *s)
+{
+	return (palrec(s, 0, strlen(s) - 1));
+}
