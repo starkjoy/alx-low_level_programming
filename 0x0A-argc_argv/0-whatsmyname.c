e4774aaa899086e1a52a6a6ee1d0563a4221c67a
stark stein <residualcode6@gmail.com>
2023-01-09 10:10:16 -0800

    Main Arguments

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - program entry
+ * @argc: accepts integer value
+ * @argv: accepts pointer to char
+ * Return: return 0
+ */
+
+int main(int argc, char* argv[])
+{
+	printf("%s\n", argv[0]);
+
+	return (0);
+}
