caa030eaea44cf387c7862c9b67ce413b2eeaf84
starkjoy <117626913+starkjoy@users.noreply.github.com>
2023-01-09 21:15:43 +0000

    Main Arguments

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -1,63 +1,54 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <ctype.h>
 
 /**
  * main - program entry
- * @argc: accepts integer value
- * @argv: accepts pointer to char
- * Return: return 0
+ * @argc: accepts integer
+ * @argv: accepts char array
+ * Return: 1 
  */
-
 int main(int argc, char *argv[])
 {
-	int i;
-	int c[5];
+	int amount;
+	int coins = 0;
 
-	int cent[] = {1, 2, 5, 10, 25};
-
-	if (argc != 1)
+	if (argc == 2)
+	{
+		amount = atoi(argv[1]);
+		if (amount < 0)
+		{
+			printf("%d\n", 0);
+			return (0);
+		}
+		if (amount % 25 >= 0)
+		{
+			coins += amount / 25;
+			amount = amount % 25;
+		}
+		if (amount % 10 >= 0)
+		{
+			coins += amount / 10;
+			amount = amount % 10;
+		}
+		if (amount % 5 >= 0)
+		{
+			coins += amount / 5;
+			amount = amount % 5;
+		}
+		if (amount % 2 >= 0)
+		{
+			coins += amount / 2;
+			amount = amount % 2;
+		}
+		if (amount % 1 >= 0)
+			coins += amount;
+		printf("%d\n", coins);
+		return (0);
+	}
+	else
 	{
 		printf("Error\n");
 		return (1);
 	}
-
-	else if (atoi(argv[1]) < 0)
-	{
-		printf("0\n");
-
-	}
-
-	else 
-	{
-		for (i = 0; i < cent[5]; i++)
-		{
-			c[0] = atoi(argv[1])/cent[0];
-			c[1] = atoi(argv[1])/cent[1];
-			c[2] = atoi(argv[1])/cent[2];
-			c[3] = atoi(argv[1])/cent[3];
-			c[4] = atoi(argv[1])/cent[4];
-		}
-
-		if (c[0] <= c[1] && c[0] <= c[2] && c[0] <= c[3] && c[0] <= c[4])
-		{
-			printf("%i\n", c[0]);
-		}
-		else if (c[1] <= c[0] && c[1] <= c[2] && c[1] <= c[3] && c[1] <= c[4])
-		{
-			printf("%i\n", c[1]);
-		}
-		else if (c[2] <= c[0] && c[2] <= c[1] && c[2] <= c[3] && c[2] <= c[4])
-		{
-			printf("%i\n", c[2]);
-		}
-		else if (c[3] <= c[0] && c[3] <= c[1] && c[3] <= c[2] && c[3] <= c[4])
-		{
-			printf("%i\n", c[3]);
-		}
-		else
-		{
-			printf("%i\n", c[4]);
-		}
-	}
-	return (0);
 }
