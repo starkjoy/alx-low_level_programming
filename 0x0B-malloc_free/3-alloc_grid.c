d64b423ddd640b2c0fab748896f69e3261bcee0d
starkjoy <residualcode6@gmail.com>
2023-01-11 16:31:28 +0000

    Malloc Free

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -12,20 +12,21 @@
 int **alloc_grid(int width, int height)
 {
 	int i;
+	int **grid;
 
 	if (width <= 0 || height <= 0)
 	{
 		return (NULL);
 	}
 
-	int **grid = (int**)malloc(height * sizeof(int));
+	**grid = (int**)malloc(height * sizeof(int));
 	if (grid == NULL)
 	{
 		return (NULL);
 	}
 
 
-	grid = (int*)malloc(width * sizeof(int))
+	grid = (int*)malloc(width * sizeof(int));
         if (grid == NULL)
         {
                 return (NULL);
