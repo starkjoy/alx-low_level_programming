dd7114507e74806b00395f1135e24516e45ad688
stark stein <residualcode6@gmail.com>
2023-01-11 02:49:59 -0800

    Meomory Functions

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -19,10 +19,12 @@ char *create_array(unsigned int size, char c)
 	{
 		return (NULL);
 	}
-
-	for (i = 0; i < size; i++)
+	else
 	{
-		ptr[i] = c;
+		for (i = 0; i < size; i++)
+		{
+			ptr[i] = c;
+		}
 	}
 
 	return ptr;
