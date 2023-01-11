dae209b7b72b48cc71001b5a5fcf8219dbaebd51
starkjoy <residualcode6@gmail.com>
2023-01-11 17:55:39 +0000

    Malloc Free

---
+++ 0x0B-malloc_free/100-argstostr.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * argstostr - joins arguments
+ * @ac: accepts integer
+ * @av: accepts pointer to pointer
+ * Return: char pointer
+ */
+
+char *argstostr(int ac, char **av)
+{
+	char *ptr;
+	int i, j;
+
+	if (ac == 0 || av == NULL)
+	{
+		return (NULL);
+	}
+
+	while (ac > 1)
+	{
+		for (i = 1; i < ac; i++)
+		{
+			ptr = (char*)malloc(sizeof(char) * (*av));
+			
+			if (av[i] == NULL)
+			{
+				int j;
+
+				for (j = 0; j < av[i]; j++)
+				{
+					free(av[j]);
+
+					return (NULL);
+				}
+			}
+		}
+		for (i = 2; i < ac; i++)
+		{
+			ptr = strcpy(ptr, av[1]);
+			ptr = strcat(ptr, "\n"av[i]);
+		}
+		putchar("\n");
+		
+		return (ptr);
+
+	}
+
+}
