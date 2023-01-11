4853b5aa23e7433a3a6a0e5266b0d41ee9b456ed
starkjoy <residualcode6@gmail.com>
2023-01-11 16:43:31 +0000

    Malloc Free

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -20,23 +20,34 @@ int **alloc_grid(int width, int height)
 	}
 
 	grid = (int**)malloc(height * sizeof(int));
+
 	if (grid == NULL)
 	{
 		return (NULL);
 	}
 
+	for (i = 0; i < height; i++)
+	{
+		grid[i] = (int**)malloc(width * sizeof(int));
+		
+		if (grid[i] == NULL)
+		{
+			int j;
 
-	grid = (int**)malloc(width * sizeof(int));
-        if (grid == NULL)
-        {
-                return (NULL);
-        }
+			for (j = 0; j < i; j++)
+			{
+				free(grid[j];
+			}
+			free(grid);
 
-
+			return (NULL);
+		}
+	}
 
 	for (i = 0; i < height; i++)
 	{
 		int j;
+
 		for (j = 0; j < width; j++)
 		{
 			grid[i][j] = 0;
