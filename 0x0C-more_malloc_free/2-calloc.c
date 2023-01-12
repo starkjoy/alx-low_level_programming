93af8ac07504d851dd7c8a942125b422ba00b080
starkjoy <residualcode6@gmail.com>
2023-01-12 17:41:12 +0000

    More Malloc Free

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * _calloc - allocates memory for an array
+ * @nmemb: accepts integer
+ * @size: accepts int
+ * Return: void
+ */
+
+void *_calloc(unsigned int nmemb, unsigned int size);
+{
+	void *ptr;
+	unsigned int width;
+
+	if (nmemb == 0 || size == 0)
+	{
+		return (NULL);
+	}
+
+	width = size * nmemb;
+
+	ptr = malloc(width);
+
+	if (ptr == NULL)
+	{
+		return (NULL);
+	}
+	else
+	{
+		memset(ptr, 0, width);
+		return (ptr);
+}
