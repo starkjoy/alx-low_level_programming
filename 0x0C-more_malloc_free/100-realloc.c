68ca4aadb712d140589d229288c4b20da328920a
starkjoy <residualcode6@gmail.com>
2023-01-12 20:08:22 +0000

    More Malloc Free

---
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * *realloc - reallocates memory using malloc and free
+ * @old_size: accepts positive int
+ * @new_size: accepts positive int
+ * @ptr: accepts pointer of any type
+ * Return: void
+ */
+
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+{
+
+	void *newp;
+
+	if (new_size == 0)
+	{
+		free(ptr);
+		return (NULL);
+	}
+
+	newp = realloc(ptr, new_size);
+	
+	if(new_ptr == NULL)
+	{
+		return (NULL);
+	}
+	return new_ptr;
+}
