f5de516e22ffd14648b14ebeafe8254cdeefcb65
starkjoy <residualcode6@gmail.com>
2023-01-12 15:22:26 +0000

    More Malloc Free

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * malloc_checked - allocates memory
+ * @b: accepts integer
+ * Return: void
+ */
+
+void *malloc_checked(unsigned int b)
+{
+	int *ptr;
+
+	ptr = malloc(sizeof(b));
+
+	if (ptr == NULL)
+	{
+		exit(98);
+	}
+}
---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,57 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * *string_nconcat - concats two strings
+ * @s1: accepts integer
+ * @s2: accepts char
+ * @n: accepts positive int
+ * Return: void
+ */
+
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	char *ptr1;
+	int size, i;
+
+	if (s1 == NULL)
+	{
+		s1 = "";
+	}
+	if (s2 == NULL)
+	{
+		s2 = "";
+	}
+	
+	size = strlen(s1) + strlen(s2);
+
+	ptr = malloc(sizeof(char) * (size + 1));
+
+	if (ptr == NULL)
+	{
+		return (NULL);
+	}
+	else
+	{
+		if (n >= strlen(s2))
+		{
+
+			for (i = 0; i < strlen[s2]; i++)
+			{
+				s1 = strcat(s1, s2[i]);
+			}
+		}
+		else
+		{
+			for (i = 0; i < n; i++)
+			{
+				s1 = strcat(s1, s2[i]);
+			}
+		}
+
+		ptr = s1;
+
+		return (ptr);
+	}
+}
