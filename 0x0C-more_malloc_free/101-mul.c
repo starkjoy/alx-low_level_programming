8fd8c7ac25cdcceadd47bf329d2664c024de0a70
starkjoy <residualcode6@gmail.com>
2023-01-13 20:24:55 +0000

    Malloc Free

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,7 +1,5 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
-#include <ctype.h>
 
 /**
  * main - program entry
@@ -13,13 +11,13 @@
 int main(int argc, char *argv[])
 {
 	int *num1, *num2;
-	long unsigned int i;
 	
 	if (argc != 3)
 	{
 		printf("Error\n");
 		exit(98);
 	}
+
 	num1 = malloc(sizeof(int));
 	num2 = malloc(sizeof(int));
 
@@ -28,36 +26,10 @@ int main(int argc, char *argv[])
 		printf("Error\n");
 		exit(98);
 	}
-	for (i = 0; i < strlen(argv[1]); i++)
-	{
-		if (!isdigit(argv[1][i]))
-		{
-		       	free(num1);
-			free(num2);
-			printf("Error\n");
-			exit(98);
-		}
-	}
-	for (i = 0; i < strlen(argv[2]); i++)
-	{
-		if (!isdigit(argv[2][i]))
-		{
-			free(num1);
-			free(num2);
-			printf("Error\n");
-			exit(98);
-		}
-	}
+
 	*num1 = atoi(argv[1]);
 	*num2 = atoi(argv[2]);
 
-	if (*num1 < 0 || *num2 < 0)
-	{
-		free(num1);
-		free(num2);
-		printf("Error\n");
-		exit(98);
-	}
 	printf("%d\n", (*num1) * (*num2));
 	free(num1);
 	free(num2);
