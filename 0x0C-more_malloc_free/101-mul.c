3d9e4792f245398f470df5d5eb91358bb5b277f2
Joy <117626913+starkjoy@users.noreply.github.com>
2023-01-13 19:49:33 +0000

    More Malloc

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -5,10 +5,18 @@
 
 int main(int argc, char *argv[])
 {
+    int *num1, *num2;
     long unsigned int i;
-    int num1, num2;
 
     if (argc != 3)
+    {
+        printf("Error\n");
+        exit(98);
+    }
+
+    num1 = malloc(sizeof(int));
+    num2 = malloc(sizeof(int));
+    if (num1 == NULL || num2 == NULL)
     {
         printf("Error\n");
         exit(98);
@@ -18,6 +26,8 @@ int main(int argc, char *argv[])
     {
         if (!isdigit(argv[1][i]))
         {
+            free(num1);
+            free(num2);
             printf("Error\n");
             exit(98);
         }
@@ -26,17 +36,23 @@ int main(int argc, char *argv[])
     {
         if (!isdigit(argv[2][i]))
         {
+            free(num1);
+            free(num2);
             printf("Error\n");
             exit(98);
         }
     }
-    num1 = atoi(argv[1]);
-    num2 = atoi(argv[2]);
-    if (num1 < 0 || num2 < 0)
+    *num1 = atoi(argv[1]);
+    *num2 = atoi(argv[2]);
+    if (*num1 < 0 || *num2 < 0)
     {
+        free(num1);
+        free(num2);
         printf("Error\n");
         exit(98);
     }
-    printf("%d\n", num1 * num2);
+    printf("%d\n", (*num1) * (*num2));
+    free(num1);
+    free(num2);
     return (0);
 }
