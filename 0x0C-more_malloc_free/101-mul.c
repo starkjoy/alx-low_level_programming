193764ddff2edfb661ad2dbe0d6d9713b422adaa
Joy <117626913+starkjoy@users.noreply.github.com>
2023-01-13 20:12:42 +0000

    More Malloc

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,58 +1,27 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
-#include <ctype.h>
 
-int main(int argc, char *argv[])
+/**
+ * main - Check the code
+ * @argc: number of arguments
+ * @argv: arguments
+ * Return: Nothing
+ */
+int main(int argc, char **argv)
 {
-    int *num1, *num2;
-    long unsigned int i;
+	int num1, num2, result;
 
-    if (argc != 3)
-    {
-        printf("Error\n");
-        exit(98);
-    }
-
-    num1 = malloc(sizeof(int));
-    num2 = malloc(sizeof(int));
-    if (num1 == NULL || num2 == NULL)
-    {
-        printf("Error\n");
-        exit(98);
-    }
-
-    for (i = 0; i < strlen(argv[1]); i++)
-    {
-        if (!isdigit(argv[1][i]))
-        {
-            free(num1);
-            free(num2);
-            printf("Error\n");
-            exit(98);
-        }
-    }
-    for (i = 0; i < strlen(argv[2]); i++)
-    {
-        if (!isdigit(argv[2][i]))
-        {
-            free(num1);
-            free(num2);
-            printf("Error\n");
-            exit(98);
-        }
-    }
-    *num1 = atoi(argv[1]);
-    *num2 = atoi(argv[2]);
-    if (*num1 < 0 || *num2 < 0)
-    {
-        free(num1);
-        free(num2);
-        printf("Error\n");
-        exit(98);
-    }
-    printf("%d\n", (*num1) * (*num2));
-    free(num1);
-    free(num2);
-    return (0);
+	if (argc != 3)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+	else
+	{
+		num1 = atoi(argv[1]);
+		num2 = atoi(argv[2]);
+		result = num1 * num2;
+		printf("%d\n", result);
+		return (0);
+	}
 }
