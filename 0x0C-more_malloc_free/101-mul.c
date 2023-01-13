35ebea9eac7ebb15bbb316ff8a1ad9b72a96295c
starkjoy <residualcode6@gmail.com>
2023-01-13 20:06:11 +0000

    Malloc Free

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -3,56 +3,64 @@
 #include <string.h>
 #include <ctype.h>
 
+/**
+ * main - program entry
+ * @argc: args count
+ * @argv: aguments array
+ * Return: int
+ */
+
 int main(int argc, char *argv[])
 {
-    int *num1, *num2;
-    long unsigned int i;
+	int *num1, *num2;
+	long unsigned int i;
 
-    if (argc != 3)
-    {
-        printf("Error\n");
-        exit(98);
-    }
+	if (argc != 3)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+	num1 = malloc(sizeof(int));
+	num2 = malloc(sizeof(int));
 
-    num1 = malloc(sizeof(int));
-    num2 = malloc(sizeof(int));
-    if (num1 == NULL || num2 == NULL)
-    {
-        printf("Error\n");
-        exit(98);
-    }
+	if (num1 == NULL || num2 == NULL)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+	for (i = 0; i < strlen(argv[1]); i++)
+	{
+		if (!isdigit(argv[1][i]))
+		{
+		       	free(num1);
+			free(num2);
+			printf("Error\n");
+			exit(98);
+		}
+	}
+	for (i = 0; i < strlen(argv[2]); i++)
+	{
+		if (!isdigit(argv[2][i]))
+		{
+			free(num1);
+			free(num2);
+			printf("Error\n");
+			exit(98);
+		}
+	}
+	*num1 = atoi(argv[1]);
+	*num2 = atoi(argv[2]);
 
-    for (i = 0; i < strlen(argv[1]); i++)
-    {
-        if (!isdigit(argv[1][i]))
-        {
-            free(num1);
-            free(num2);
-            printf("Error\n");
-            exit(98);
-        }
-    }
-    for (i = 0; i < strlen(argv[2]); i++)
-    {
-        if (!isdigit(argv[2][i]))
-        {
-            free(num1);
-            free(num2);
-            printf("Error\n");
-            exit(98);
-        }
-    }
-    *num1 = atoi(argv[1]);
-    *num2 = atoi(argv[2]);
-    if (*num1 < 0 || *num2 < 0)
-    {
-        free(num1);
-        free(num2);
-        printf("Error\n");
-        exit(98);
-    }
-    printf("%d\n", (*num1) * (*num2));
-    free(num1);
-    free(num2);
-    return (0);
+	if (*num1 < 0 || *num2 < 0)
+	{
+		free(num1);
+		free(num2);
+		printf("Error\n");
+		exit(98);
+	}
+	printf("%d\n", (*num1) * (*num2));
+	free(num1);
+	free(num2);
+
+	return (0);
 }
