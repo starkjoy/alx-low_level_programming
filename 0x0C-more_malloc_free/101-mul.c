d44543b9afa35a89558eb7a5ae3b6d81866b34f9
Joy <117626913+starkjoy@users.noreply.github.com>
2023-01-13 19:33:47 +0000

    More Malloc

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,51 +1,41 @@
 #include <stdio.h>
 #include <stdlib.h>
-
-/**
- * main - program entry
- * @argv: accepts a char array
- * @argc: accepts an int
- * Return: pointer to integer
- */
+#include <string.h>
+#include <ctype.h>
 
 int main(int argc, char *argv[])
 {
-	int *ptr1;
-	int *ptr2;
+    int num1, num2, i;
 
-	if (argc != 3)
-	{
-		printf("Error\n");
-		exit(98);
-	}
+    if (argc != 3)
+    {
+        printf("Error\n");
+        exit(98);
+    }
 
-	ptr1 = malloc(sizeof(int));
-	ptr2 = malloc(sizeof(int));
-
-	if (ptr1 == NULL || ptr2 == NULL)
-	{
-		free(ptr1);
-		free(ptr2);
-		printf("Error\n");
-		exit(98);
-	}
-	else
-	{
-		if (atoi(argv[1]) >= 0 || atoi(argv[2]) >= 0)
-		{
-			printf("Error\n");
-			exit(98);
-		}
-		else
-		{
-			*ptr1 = atoi(argv[1]);
-			*ptr2 = atoi(argv[2]);
-
-			printf("%i\n", (*ptr1) * (*ptr2));
-		}
-	}
-	free(ptr1);
-	free(ptr2);
-
-	return (0);
+    for (i = 0; i < strlen(argv[1]); i++)
+    {
+        if (!isdigit(argv[1][i]))
+        {
+            printf("Error\n");
+            exit(98);
+        }
+    }
+    for (i = 0; i < strlen(argv[2]); i++)
+    {
+        if (!isdigit(argv[2][i]))
+        {
+            printf("Error\n");
+            exit(98);
+        }
+    }
+    num1 = atoi(argv[1]);
+    num2 = atoi(argv[2]);
+    if (num1 < 0 || num2 < 0)
+    {
+        printf("Error\n");
+        exit(98);
+    }
+    printf("%d\n", num1 * num2);
+    return (0);
 }
