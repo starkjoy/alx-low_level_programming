6aca23764e355d65657267362cc6ad4f2c26dd5e
starkjoy <residualcode6@gmail.com>
2023-01-16 18:09:11 +0000

    Structures and Typedef

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,5 +1,7 @@
 #include "dog.h"
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 /**
  * new_dog - creates a dog_t type struct
@@ -11,12 +13,32 @@
 
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	if (newdog == NULL)
+	dog_t *ndog = malloc(sizeof(dog_t));
+	if (ndog == NULL)
+	{
 		return (NULL);
-	
-	newdog->name = name;
-	newdog->age = age;
-	newdog->owner = owner;
+	}
 
-	return (newdog);
+	ndog->name = malloc(strlen(name) + 1);
+	if (ndog->name == NULL)
+	{
+		free(ndog);
+		return (NULL);
+	}
+	strcpy(ndog->name, name);
+	
+	ndog->age = age;
+	
+	ndog-owner = malloc(strlen(owner) + 1);
+
+	if (ndog->owner == NULL)
+	{
+		free(ndog->name);
+		free(ndog);
+
+		return (NULL);
+	}
+	strcpy(ndog->owner, owner);
+
+	return (ndog);
 }
