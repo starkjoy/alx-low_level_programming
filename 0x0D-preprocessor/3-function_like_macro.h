36bb1af804b9e225104f9f1aad1ce1bb47618695
starkjoy <residualcode6@gmail.com>
2023-01-16 14:17:40 +0000

    C-preprocessors

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,2 +1,5 @@
+#ifndef 3-FUNCTION_LIKE_MACRO_H
+#define 3-FUNCTION_LIKE_MACRO_H
 #define ABS(x) i((x < 0) ? -(x) : x)
+#endif
 
--- 0x0D-preprocessor/4-sum.h
+++ 0x0D-preprocessor/4-sum.h
@@ -1,1 +1,5 @@
+#ifndef 4-SUM_H
+#define 4-SUM_H
 #define SUM(x, y) (x) + (y)
+#endif
+
