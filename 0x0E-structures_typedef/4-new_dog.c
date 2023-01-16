28233dc9540707e8fc71f77468776792c0d0aeeb
starkjoy <residualcode6@gmail.com>
2023-01-16 18:19:50 +0000

    Structures and Typedef

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -14,21 +14,23 @@
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *ndog = malloc(sizeof(dog_t));
+
 	if (ndog == NULL)
 	{
 		return (NULL);
 	}
 
 	ndog->name = malloc(strlen(name) + 1);
+
 	if (ndog->name == NULL)
 	{
 		free(ndog);
 		return (NULL);
 	}
 	strcpy(ndog->name, name);
-	
+
 	ndog->age = age;
-	
+
 	ndog->owner = malloc(strlen(owner) + 1);
 
 	if (ndog->owner == NULL)
