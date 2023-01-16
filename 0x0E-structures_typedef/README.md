2339fa8d381761a73cdaca25281eb85f8ec936a5
starkjoy <residualcode6@gmail.com>
2023-01-16 15:04:42 +0000

    Structures and Typedef

---
+++ 0x0E-structures_typedef/README.md
@@ -0,0 +1,3 @@
+# About
+---
+This folder contains lesson files on structures and typedefs
---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * dog - stores dog traits
+ * @name: accepts string
+ * @age: accepts float
+ * @owner: accepts string
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
