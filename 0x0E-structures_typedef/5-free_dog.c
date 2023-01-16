246dce6aa47f388c62f2b5a0b42aca5083ec4ac9
starkjoy <residualcode6@gmail.com>
2023-01-16 18:56:00 +0000

    Structures and Typedef

--- 0x0E-structures_typedef/5-free_dog.c
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -7,13 +7,16 @@
  * Return: void
  */
 
-void free(dog_t *d)
+void free_dog(dog_t *d)
 {
 	if (d == NULL)
 		return;
+
 	if (d->name != NULL)
 		free(d->name);
+
 	if (d->owner != NULL)
 		free(d->owner);
+
 	free(d);
 } 
