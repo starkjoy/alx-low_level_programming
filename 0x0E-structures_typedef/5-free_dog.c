9a73e058b157909bff697d0521ef48b2e2ea2bfc
starkjoy <residualcode6@gmail.com>
2023-01-16 18:30:55 +0000

    Structures and Typedef

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,16 @@
+#include "dog.h"
+#include <stdio.h>
+
+/**
+ * free_dog - free dogs
+ * @d: accepts struct of dog_t
+ * Return: void
+ */
+
+void free(dog_t *d)
+{
+	free(d->name);
+	free(d->owner);
+	free(d->age);
+	free(d);
+}
