c934e6baa4160e84f0a931fdc5160758d0ae5bc8
starkjoy <residualcode6@gmail.com>
2023-01-16 17:29:32 +0000

    Structures and Typedef

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 
 /**
- * new_dog - initializes a variable
+ * new_dog - creates a dog_t type struct
  * @name: accepts string
  * @age: accepts float
  * @owner: accepts string
@@ -11,15 +11,14 @@
 
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	struct dog_t *ndogs
-	{
-		ndogs->name = name;
-		ndogs->age = age;
-		ndogs->owner = owner;
-	}
+	dog_t *ndogs;
 
 	if (ndogs == NULL)
 		return (NULL);
+	
+	ndogs->name = name;
+	ndogs->age = age;
+	ndogs->owner = owner;
 
 	return (ndogs);
 }
