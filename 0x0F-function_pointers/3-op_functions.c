893c2f81fefb3806af507915d9324fa136f796cb
stark stein <residualcode6@gmail.com>
2023-01-18 09:53:22 -0800

    Function Pointers

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 /**
  * op_add - returns sum
@@ -45,7 +46,12 @@ int op_mul(int a, int b)
 
 int op_div(int a, int b)
 {
-	return (a * b);
+	if (b == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
+	return (a / b);
 }
 
 /**
@@ -57,5 +63,10 @@ int op_div(int a, int b)
 
 int op_mod(int a, int b)
 {
+	if (b == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
 	return (a % b);
 }
