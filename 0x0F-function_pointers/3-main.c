0ab76246057103f46cd27b8d109813772ca9faf2
stark stein <residualcode6@gmail.com>
2023-01-18 09:30:01 -0800

    Function Pointers

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -1,12 +1,12 @@
 #include "3-calc.h"
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
 
 int main(int argc, char *argv[])
 {
 	int num1;
 	int num2;
-	char operator;
 
 	if (argc < 5)
 	{
@@ -14,21 +14,20 @@ int main(int argc, char *argv[])
 		exit(98);
 	}
 
-	if (argv[2] != '+' || argv[2] != '-' || argv[2] != '*' || argv[2] != '/' || argv[2] != '%')
+	if (strcmp(argv[2], "+") != 0 && strcmp(argv[2], "-") != 0 && strcmp(argv[2], "*") != 0 && strcmp(argv[2], "/") != 0 && strcmp(argv[2], "%") != 0)
 	{
 		printf("Error\n");
 		exit(99);
 	}
 
-	if (argv[1] == '0' && argv[2] == '/' || argv[1] == '0' && argv[2] == '%')
+	if ((strcmp(argv[1], "0") == 0 && strcmp(argv[2], "/") == 0) || (strcmp(argv[1], "0") == 0 && strcmp(argv[2], "%") == 0))
 	{
 		printf("Error\n");
 		exit(100);
 	}
 
 	num1 = atoi(argv[1]);
-	operator = argv[2];
 	num2 = atoi(argv[3]);
 
-	return (get_op_func(operator)(num1, num2));
+	return (get_op_func(argv[2])(num1, num2));
 }
