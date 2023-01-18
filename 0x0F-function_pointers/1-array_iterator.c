80c00fdee6b29c4c6ad35ef249f8d46ffc1c4d84
stark stein <residualcode6@gmail.com>
2023-01-18 04:48:05 -0800

    Function Pointers

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * array_iterator
+ * @array: accepts integer array
+ * @size: accepts struct size_t
+ * @action: accepts fuction
+ * Return: void
+ */
+
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	int i;
+
+	if (array == NULL || action == NULL || size == NULL)
+	{
+		return;
+	}
+
+	for (i = 0; i < size; i++)
+	{
+		action(array[i]);
+	}
+}
