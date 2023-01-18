398454b4385f4f4b18b406194ad67a69d66743a9
stark stein <residualcode6@gmail.com>
2023-01-18 09:47:21 -0800

    Function Pointers

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -3,31 +3,30 @@
 #include <stdio.h>
 #include <string.h>
 
+/**
+ * main - program entry
+ * @argc: args count
+ * @argv: array of args
+ * Return: integer
+ */
+
 int main(int argc, char *argv[])
 {
-	int num1;
-	int num2;
+	int (func)(int, int);
 
-	if (argc < 5)
+	if (argc != 4)
 	{
 		printf("Error\n");
 		exit(98);
 	}
 
-	if (strcmp(argv[2], "+") != 0 && strcmp(argv[2], "-") != 0 && strcmp(argv[2], "*") != 0 && strcmp(argv[2], "/") != 0 && strcmp(argv[2], "%") != 0)
+	func = get_op_func(argv[2]);
+	if (func == NULL)
 	{
 		printf("Error\n");
 		exit(99);
 	}
 
-	if ((strcmp(argv[1], "0") == 0 && strcmp(argv[2], "/") == 0) || (strcmp(argv[1], "0") == 0 && strcmp(argv[2], "%") == 0))
-	{
-		printf("Error\n");
-		exit(100);
-	}
-
-	num1 = atoi(argv[1]);
-	num2 = atoi(argv[3]);
-
-	return (get_op_func(argv[2])(num1, num2));
+	printf("%d\n", func(atoi(argv[1]), atoi(argv[3])));
+	return (0);
 }
