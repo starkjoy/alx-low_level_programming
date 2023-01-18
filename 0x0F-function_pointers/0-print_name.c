e7acc6d6d8e5ea8918d95b2f8d82dfebf905d58f
stark stein <residualcode6@gmail.com>
2023-01-18 04:36:14 -0800

    Function Pointers

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-#include "function_pointers.h"
 
 /**
  * print_name - prints a name
@@ -10,6 +9,10 @@
 
 void print_name(char *name, void (*f)(char *))
 {
+	if (name == NULL || f == NULL)
+	{
+		return;
+	}
+
 	f(name);
-	_putchar("\n");
 }
