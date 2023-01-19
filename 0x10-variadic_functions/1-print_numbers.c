f719a7e77d545e61ac473b582e740a49dc7ffb8f
stark stein <residualcode6@gmail.com>
2023-01-19 07:00:48 -0800

    Variadic Functions

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -13,19 +13,16 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	unsigned int i;
 	va_list args;
 
-	if (separator == NULL)
-	{
-		return;
-	}
-
 	va_start(args, n);
 
 	for (i = 0; i < n; i++)
 	{
-		printf("%i", va_arg(args, int));
-		printf("%s", separator);
+		printf("%d", va_arg(args, int));
+		if (separator == NULL)
+			continue;
+		if (i < n - 1)
+			printf("%s", separator);
 	}
 	putchar('\n');
-
 	va_end(args);
 }
