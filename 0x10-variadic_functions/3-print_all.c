a9c4da6719bf3a92468749d2bc9423abfffc0eec
starkjoy <residualcode6@gmail.com>
2023-01-19 14:38:12 +0000

    Variadic Functions

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -3,22 +3,50 @@
 
 /**
  * print_all - prints anything
- * @format: parametershhhh
+ * @format: parameters
  * Return: void
- */hh
-h
-vhoid print_all(const char * const format, ...)
-{
-hh	va_list args;
-h
-h	va_start(args, format);
-h
-h	int i = va_arg(args, int);
-hh	float f = va_arg(args, float);
-h	char c = va_arg(args, char);
-	char *s = va_arg(args, char *);
-h
-h	putchar('\n');
+ */
 
-hh	va_end(args;
-}hhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhh
+void print_all(const char * const format, ...)
+{
+	va_list args;
+	int i = 0;
+	char *sep = "";
+
+	va_start(args, format);
+
+	while (format == NULL)
+	{
+		printf("\n");
+		return;
+	}
+
+	while (format[i])
+	{
+		switch (format[i])
+		{
+			case 'c':
+				printf("%s%c", sep, (char)va_arg(args, int));
+				break;
+			case 'i':
+				printf("%s%d", sep, va_arg(args, int));
+				break;
+			case 'f':
+				printf("%s%f", sep va_arg(args, double));
+				break;
+			case 's':
+				if (va_arg(args, char *) != NULL)
+				{
+					printf("%s%s", sep, va_arg(args, char *));
+					break;
+				}
+				printf("(nil)");
+				break;
+		}
+		sep = ",";
+		i++;
+	}
+
+	putchar('\n');
+	va_end(args);
+}
