0373ee8526e8504729e1f1f6149cb7e3074658b7
stark stein <residualcode6@gmail.com>
2023-01-26 08:26:37 -0800

    Linked list

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * free_list - free list
+ * @head: accepts pointer to pointer
+ * Return: returns void
+ */
+
+void free_list(list_t *head)
+{
+	list_t temp;
+
+	while(head != NULL)
+	{
+		temp = head;
+		head = head->next;
+		free(temp->str);
+		free(temp);
+	}
+}
