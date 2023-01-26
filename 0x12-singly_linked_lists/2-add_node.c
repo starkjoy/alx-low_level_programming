455d231a258e8c07df98c9181f61d6102965aafe
stark stein <residualcode6@gmail.com>
2023-01-26 07:09:25 -0800

    Linked list

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -9,7 +9,7 @@
  * Return: returns variable of type list_t
  */
 
-list_t *add_node(list_t **head, const char *str);
+list_t *add_node(list_t **head, const char *str)
 {
 	list_t *new_node = malloc(sizeof(list_t));
 
