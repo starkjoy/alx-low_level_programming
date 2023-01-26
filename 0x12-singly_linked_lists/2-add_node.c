349a1358d9b19a04e3830d4c7181625cf8a42d0a
stark stein <residualcode6@gmail.com>
2023-01-26 07:20:09 -0800

    Linked list

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -12,12 +12,21 @@
 
 list_t *add_node(list_t **head, const char *str)
 {
-	list_t *new_node = malloc(sizeof(list_t));
+	list_t *new_node;
+	int length = 0;
 
+	new_node = malloc(sizeof(list_t));
 	if (new_node == NULL)
 	{
 		return (NULL);
 	}
+
+	while (str[length])
+	{
+		length++;
+	}
+
+	new_node->len = length;
 	new_node->str = strdup(str);
 	new_node->next = *head;
 	*head = new_node;
