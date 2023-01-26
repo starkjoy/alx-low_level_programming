dccaaffb2320e63ca5594f13cc791970ef2bae65
stark stein <residualcode6@gmail.com>
2023-01-26 05:25:37 -0800

    Linked Lists

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,30 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * print_list - prints all elements of a list_t
+ * @h: accepts pointer to list_t
+ * Return: returns type size_t
+ */
+
+size_t print_list(const list_t *h)
+{
+	size_t n;
+
+	if (str == NULL)
+	{
+		printf("[0] (nil)");
+	}
+	else
+	{
+		printf("[%u] (%s)\n", len, str);
+	}
+
+	n = print_list(head);
+
+	printf("-> %lu elements\n", n);
+
+	return (n);
+}
