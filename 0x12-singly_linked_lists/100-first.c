fae9622237e52968e27270a01f33084168f4475e
stark stein <residualcode6@gmail.com>
2023-01-26 08:55:13 -0800

    Linked list

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -7,7 +7,7 @@
 
 int main(void)
 {
-       	printf("(A tortoise, having pretty good sense of a hare's nature, challenges one to a race.)\n");
+       	printf("You' re beat! and yet, you must allow,\nI bore my house upon my back!\n");
 
 	return (0);
 
