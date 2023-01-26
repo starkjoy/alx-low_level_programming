99b78ed4a33587ff0e21d841b6ae169c0ed9ee81
stark stein <residualcode6@gmail.com>
2023-01-26 09:16:03 -0800

    Linked list

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -7,7 +7,7 @@ void main_constructor(void) __attribute_((constructor));
  * Returns: void
  */
 
-void main_contructor(void)
+void main_constructor(void)
 {
 	printf("You're beat! and yet, you must allow,\n");
 	printf("I bore my house upon my back!\n");
