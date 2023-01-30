88651cdc4f175d3a6f8dd2e626c4d00340d07fc2
stark stein <residualcode6@gmail.com>
2023-01-30 10:20:49 -0800

    More Singly Linked Lists

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -2,11 +2,12 @@
 #define LISTS_H
 
 /**
- * struct list_s - singly linked list
+ * struct listint_s - singly linked list
  * @n: integer
  * @next: points to the next node
  *
  * Description: singly linked list node structure
+ *
  */
 
 typedef struct listint_s
