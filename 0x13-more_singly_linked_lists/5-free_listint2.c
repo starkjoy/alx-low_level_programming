d38eb24b5aed5db5e711ef1b6f49fce0aeb09404
stark stein <residualcode6@gmail.com>
2023-01-30 10:55:41 -0800

    More Singly Linked Lists

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -11,14 +11,17 @@
 
 void free_listint2(listint_t **head)
 {
-	listint_t **temp;
+	listint_t *temp;
+
+	if (head == NULL)
+		return;
 
 	temp = *head;
 	while (*head)
 	{
 		temp = *head;
-		*head = NULL;
-		free(*temp);
+		(*head) = (*head)->next;
+		free(temp);
 	}
-	free(*head);
+	*head = NULL;
 }
