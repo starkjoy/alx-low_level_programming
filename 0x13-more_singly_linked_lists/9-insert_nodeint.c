3f3b909afb1d70e25697f68f659855f52de3b1e7
stark stein <residualcode6@gmail.com>
2023-01-30 11:51:31 -0800

    More Singly Linked Lists

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -21,7 +21,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	{
 		return (NULL);
 	}
-	
+
 	new_node->n = n;
 	new_node->next = NULL;
 
@@ -37,7 +37,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		current = current->next;
 		count++;
 	}
-	
+
 	if (current == NULL)
 	{
 		return (NULL);
