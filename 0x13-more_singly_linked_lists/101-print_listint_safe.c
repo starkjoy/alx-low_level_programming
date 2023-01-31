b15ca284fee9b6142695ca3d41231b4b90aca433
starkjoy <residualcode6@gmail.com>
2023-01-31 10:32:17 +0000

    More Linked Lists

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -24,7 +24,7 @@ size_t print_listint_safe(const listint_t *head)
 
 		if (temp == fast)
 		{
-			printf("-> [%p] %d\n", (void *)temp, temp->n);
+			printf("-> [%p] %d\n", (void *)head, head->n);
 			exit(98);
 		}
 
