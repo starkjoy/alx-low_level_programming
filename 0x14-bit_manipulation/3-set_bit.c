f373f400f3dd0fb621ff26937e13707007956cf0
starkjoy <residualcode6@gmail.com>
2023-02-02 16:14:50 +0000

    Bit Manipulations

---
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * set_bit - gives the value of a bit at a given index
+ * @n: accepts unsigned long int
+ * @index: accepts unsigned long int
+ * Return: returns an int
+ */
+
+int set_bit(unsigned long int *n, unsigned int index)
+{
+	if (!n || index > (sizeof(n) * 8 - 1))
+	{
+		return (-1);
+	}
+	*n = *n | (1 << index);
+	return (1);
+}
