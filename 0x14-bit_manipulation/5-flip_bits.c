c7f5058f9bfa8836b2d7a5101356b3ca7a83c768
starkjoy <residualcode6@gmail.com>
2023-02-02 17:35:22 +0000

    Bit Manipulations

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -1,20 +1,20 @@
 #include <stdio.h>
 
 /**
- * flip_bit - returns number of bits to flip
+ * flip_bits - returns number of bits to flip
  * @n: accepts unsigned long int
  * @m: accepts unsigned long int
  * Return: returns an unsigned int
  */
 
-unsigned int flip_bits(unsigned long int n, unsigned int m)
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
 {
 	unsigned int count = 0;
 	unsigned long int x = n ^ m;
 
 	while (x)
 	{
-		count = count + (x & 1);
+		count = count + x & 1;
 		x = x >> 1;
 	}
 	return (count);
