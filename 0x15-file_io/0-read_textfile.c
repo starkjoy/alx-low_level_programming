8f84e5c9be5b6b087930a82715800adf5240fc9d
stark stein <residualcode6@gmail.com>
2023-02-06 04:38:54 -0800

    File I/O

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * read_textfile - reads a textfile and prints to STDOUT
+ * @filename: accepts string
+ * @letters: accepts integer
+ * Return: returns void
+ */
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+
+}
