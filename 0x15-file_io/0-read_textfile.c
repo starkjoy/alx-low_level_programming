eb4d0569925cce194be204f9ff347bb6f8bdc635
stark stein <residualcode6@gmail.com>
2023-02-06 05:03:55 -0800

    File I/O

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -20,21 +20,21 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	{
 		return (0);
 	}
+
+	buffer = malloc(sizeof(char) * letters);
+	if (buffer == NULL)
+		return (0);
+
 	fd = open(filename, O_RDONLY);
-	if (fd == -1)
+	if (fd < 0)
 	{
+		free(buffer);
 		return (0);
 	}
 
-	buffer = malloc(sizeof(*buffer) * letters);
-	if (buffer == NULL)
-	{
-		close(fd);
-		return (0);
-	}
 
 	read_bytes = read(fd, buffer, letters);
-	if (read_bytes == -1)
+	if (read_bytes < 0)
 	{
 		free(buffer);
 		close(fd);
@@ -42,7 +42,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	}
 
 	write_bytes = write(STDOUT_FILENO, buffer, read_bytes);
-	if (write_bytes == -1 || write_bytes != read_bytes)
+	if (write_bytes < 0)
 	{
 		free(buffer);
 		close(fd);
