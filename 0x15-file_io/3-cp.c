e6c742cd82c65c35ad25374335027d0481889f6b
starkjoy <residualcode6@gmail.com>
2023-02-07 03:06:55 -0800

    File I/O

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -52,7 +52,7 @@ int main(int argc, char *argv[])
 
 	if (read_from == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %\n", argv[1]);
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		exit(98);
 	}
 
