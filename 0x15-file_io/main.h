da7d287018d5185f035ba994ac8cb565af5817f9
starkjoy <residualcode6@gmail.com>
2023-02-07 08:10:16 -0800

    File I/O

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,8 +1,13 @@
-#ifndef MAIN_H
-#define MAIN_h
-
+#ifndef BUFSIZE
 #define BUFSIZE 1024
+#endif
+
+#ifndef ERROR_MSG_LEN
 #define ERROR_MSG_LEN 32
+#endif
+
+#ifndef MAIN_H
+#define MAIN_H
 
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
