77900d07f73181df7905e0196e4a924c3d3f4dae
starkjoy <residualcode6@gmail.com>
2023-02-07 07:50:59 -0800

    File I/O

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,64 +1,49 @@
 #include <stdio.h>
-#include <stdlib.h>
 #include <fcntl.h>
 #include <unistd.h>
-#define BUFSIZE 1024
+#include "main.h"
 
 /**
  * main - copies data from file to file
  * @argc: agruments count
  * @argv: arguments passed
- * Return: returns int
+ * Return: returns an int
  */
 
-int main(int argc, char *argv[])
-{
-	int fd_from, fd_to, close_from, close_to;
-	char buf[BUFSIZE];
-	ssize_t read_from, write_to;
+int open_file(const char *file, int flags);
 
+int main (int argc, char *argv[])
+{
 	if (argc != 3)
 	{
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 		exit(97);
 	}
-	fd_from = open(argv[1], O_RDONLY);
-	if (fd_from == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
-		exit(98);
-	}
-	fd_to = open(argv[2], O_WRONLY | O_TRUNC | O_CREAT, 0664);
-	if (fd_to == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
-		exit(99);
-	}
-	while ((read_from = read(fd_from, buf, BUFSIZE)) > 0)
-	{
-		write_to = write(fd_to, buf, read_from);
-		if (write_to == -1)
-		{
-			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
-			exit(99);
-		}
-	}
-	if (read_from == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
-		exit(98);
-	}
-	close_from = close(fd_from);
-	if (close_from == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_from);
-		exit(100);
-	}
-	close_to = close(fd_to);
-	if (close_to == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_to);
-		exit(100);
-	}
+
+	int fd_from = open_file(argv[1], 0_RDONLY);
+	int fd_to = open_file(argv[2], O_WRONLY | O_TRUNC | O_CREAT);
+
+	copy_file(fd__from, fd_to);
+
+	if (close(fd_from) == -1)
+		error_exit(100, "close fd");
+	if (close(fd_to) == -1)
+		error_exit(100, "close fd");
+
 	return (0);
+}
+
+/**
+ * open_file - opens file
+ * @file: file name
+ * @flags: accepts integer
+ * Return: Integer;
+ */
+
+int open_file(const char *file, int flags)
+{
+	int fd = open(file, flags, 0664);
+	if (fd == -1)
+		error_exit(errno == ENOENT ? 98 : 99, file);
+	return (fd);
 }
