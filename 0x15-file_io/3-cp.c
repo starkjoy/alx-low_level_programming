fda1ef56a5814a39a4a289be0f26a6f4d90c13ba
starkjoy <residualcode6@gmail.com>
2023-02-07 07:58:14 -0800

    File I/O

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -16,14 +16,16 @@ int open_file(const char *file, int flags);
 
 int main (int argc, char *argv[])
 {
+	int fd_from, fd_to;
+
 	if (argc != 3)
 	{
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 		exit(97);
 	}
 
-	int fd_from = open_file(argv[1], O_RDONLY);
-	int fd_to = open_file(argv[2], O_WRONLY | O_TRUNC | O_CREAT);
+	fd_from = open_file(argv[1], O_RDONLY);
+	fd_to = open_file(argv[2], O_WRONLY | O_TRUNC | O_CREAT);
 
 	copy_file(fd_from, fd_to);
 
