42f5436b1831acdff0ff0ee6a13ba4c167a07155
starkjoy <residualcode6@gmail.com>
2023-02-07 20:11:02 +0000

    File I/O

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -13,22 +13,29 @@ int main(int argc, char *argv[])
 	Elf64_Ehdr elf_header;
 
 	if (argc != 2)
-		fprintf(stderr, "Usage: %s elf_filename\n", argv[0]), return (98);
-
+	{
+		fprintf(stderr, "Usage: %s elf_filename\n", argv[0]);
+		return (98);
+	}
 	if ((fd = open(argv[1], O_RDONLY)) < 0)
-		perror("open"), return (98);
-
+	{
+		perror("open");
+		return (98);
+	}
 	if (read(fd, &elf_header, sizeof(Elf64_Ehdr)) != sizeof(Elf64_Ehdr))
-		fprintf(stderr, "%s is not an ELF file\n", argv[1]), return (98);
-
+	{
+		fprintf(stderr, "%s is not an ELF file\n", argv[1]);
+		return (98);
+	}
 	if (elf_header.e_ident[EI_MAG0] != ELFMAG0 ||
 			elf_header.e_ident[EI_MAG1] != ELFMAG1 ||
 			elf_header.e_ident[EI_MAG2] != ELFMAG2 ||
 			elf_header.e_ident[EI_MAG3] != ELFMAG3)
-		fprintf(stderr, "%s is not an ELF file\n", argv[1]), return (98);
-
+	{
+		fprintf(stderr, "%s is not an ELF file\n", argv[1]);
+		return (98);
+	}
 	printf("ELF Header: \n");
-
 	printf(" Magic:  %02x %02x %02x %02x\n", elf_header.e_ident[EI_MAG0],
 			elf_header.e_ident[EI_MAG1], elf_header.e_ident[EI_MAG2],
 			elf_header.e_ident[EI_MAG3]);
@@ -40,6 +47,5 @@ int main(int argc, char *argv[])
 	printf(" ABI Version:             %d\n", elf_header.e_ident[EI_ABIVERSION]);
 	printf(" Type:                    %d\n", elf_header.e_type);
 	printf(" Entry point address:     %ld\n", elf_header.e_entry);
-
 	return (0);
 }
