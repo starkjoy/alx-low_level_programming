cc75e0ffc82b0658925ec0d4abffd1fb515845a7
starkjoy <residualcode6@gmail.com>
2023-03-16 10:38:15 -0700

    Doubly Linked List

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -9,11 +9,13 @@
 size_t print_dlistint(const dlistint_t *h)
 {
 	size_t count_nodes = 0;
+	const dlistint_t *temp;
 
-	while (h != NULL)
+	temp = h;
+	while (temp)
 	{
-		printf("%d/n", h->n);
-		h = h->next;
+		printf("%d/n", temp->n);
+		temp = temp->next;
 		count_nodes++;
 	}
 
