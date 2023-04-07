9778e90fc821a9751bae30f761d3197872f6455b
starkjoy <residualcode6@gmail.com>
2023-04-07 02:04:01 -0700

    Hash Tables

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -12,17 +12,13 @@ hash_table_t *hash_table_create(unsigned long int size)
 
 	table = (hash_table_t *)malloc(sizeof(hash_table_t));
 	if (!(table))
+		return (NULL);
+	table->array = calloc(size, sizeof(hash_node_t *));
+	if (!table->array)
 	{
+		free(table);
 		return (NULL);
 	}
-	else
-	{
-		table->array = (hash_node_t **)calloc(size, sizeof(hash_node_t *));
-		if (!(table->array))
-		{
-			free(table);
-			return (NULL);
-		}
-		return (table);
-	}
+	table->size = size;
+	return (table);
 }
