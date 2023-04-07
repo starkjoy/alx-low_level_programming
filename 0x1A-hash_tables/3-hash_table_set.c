76360fec895d26f10a6f4484342b50e1cec8e6ab
starkjoy <residualcode6@gmail.com>
2023-04-07 02:44:48 -0700

    Hash Tables

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -13,37 +13,41 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	unsigned long int index;
 	hash_node_t *new_node, *current_node;
 
-	if (ht == NULL || key == NULL || *key == '\0')
+	if (!ht || !key || !*key || !value)
 		return (0);
 
 	index = key_index((const unsigned char *)key, ht->size);
+	current_node = ht->array[index];
+
+	while (current_node != NULL)
+	{
+		if (strcmp(current_node->key, key) == 0)
+		{
+			free(current_node->value);
+			current_node->value = strdup(value);
+			if (!current_node->value)
+				return (0);
+			return (1);
+		}
+		current_node = current_node->next;
+	}
 	new_node = malloc(sizeof(hash_node_t));
-	if (new_node == NULL)
+	if (!new_node)
 		return (0);
 	new_node->key = strdup(key);
-	if (new_node->key == NULL)
+	if (!new_node->key)
 	{
 		free(new_node);
 		return (0);
 	}
 	new_node->value = strdup(value);
-	if (new_node->value == NULL)
+	if (!new_node->value)
 	{
 		free(new_node->key);
 		free(new_node);
 		return (0);
 	}
-	new_node->next = NULL;
-
-	current_node = ht->array[index];
-	if (current_node == NULL)
-	{
-		ht->array[index] = new_node;
-	}
-	else
-	{
-		new_node->next = current_node;
-		ht->array[index] = new_node;
-	}
+	new_node->next = ht->array[index];
+	ht->array[index] = new_node;
 	return (1);
 }
