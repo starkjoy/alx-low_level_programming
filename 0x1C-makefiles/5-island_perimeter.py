d612bb0337f3ff1698b9d83ef736870e325d07be
Joy <117626913+starkjoy@users.noreply.github.com>
2023-05-24 15:05:10 +0000

    Update 5-island_perimeter.py

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -13,19 +13,23 @@ def island_perimeter(grid):
     - Mapped matrix using row and column length
     - Navigated matrix using square bracket notations
     '''
-    
+    peri = 0
     rows = len(grid)
     cols = len(grid[0])
-    perimeter = 0
-    for i in range(1, rows - 1):
-        for j in range(1, cols - 1):
+
+    for i in range(rows):
+        for j in range(cols):
             if grid[i][j] == 1:
-                if grid[i - 1][j] == 0:
-                    perimeter += 1
-                if grid[i + 1][j] == 0:
-                    perimeter += 1
-                if grid[i][j - 1] == 0:
-                    perimeter += 1
-                if grid[i][j + 1] == 0:
-                    perimeter += 1
-    return perimeter
+                peri += 4  # Add 4 to the perimeter for each land cell
+
+                # Check neighboring cells
+                if i > 0 and grid[i - 1][j] == 1:
+                    peri -= 2  # Subtract 2 if the cell above is also land
+                if j > 0 and grid[i][j - 1] == 1:
+                    peri -= 2  # Subtract 2 if the cell on the left is also land
+                if i < rows - 1 and grid[i + 1][j] == 1:
+                    peri -= 2  # Subtract 2 if the cell below is also land
+                if j < cols - 1 and grid[i][j + 1] == 1:
+                    peri -= 2  # Subtract 2 if the cell on the right is also land
+
+    return peri
