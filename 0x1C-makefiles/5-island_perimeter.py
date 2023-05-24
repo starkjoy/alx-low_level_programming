28e4b53514423cd673b6d35f5a5250fa2b157b49
starkjoy <residualcode6@gmail.com>
2023-05-24 08:23:30 -0700

    Makefiles

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -24,8 +24,8 @@ def island_perimeter(grid):
         for j in range(cols):
             if grid[i][j] == 1:
                 perimter += 4
-                if (j > 0 and grid[i][j - 1] == 1):
+                if i > 0 and grid[i - 1][j] == 1:
                     perimeter -= 2
-                if (i > 0 and grid[i - 1][j] == 1):
+                if j > 0 and grid[i][j - 1] == 1:
                     perimeter -= 2
     return perimeter
